7e9395495346a7de83ab4b27269ae946c907c313
Linkan Patra <your-email@example.com>
2026-04-06 12:27:53 +0530

    message

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 5050;
-const connectDB = require('')
+const connectDB = require('./config/dbConnection.js')
+
+// Middleware
+app.use(express.json());
+app.use("/uploads", express.static('uploads'));
+app.use("/api", require("./routes/book.routes.js"))
+
+connectDB();
+
+app.listen(port, () => {
+    console.log("Server running on http://localhost:5050"); 
+});
